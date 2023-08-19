fed34b7c990c6d349bc063f5b124ff64d71c4a3c
yogadwiprasetyo <jobsforyoga@gmail.com>
2023-08-19 14:56:01 +0700

    fix(realm-swift): using specific version 10,42,0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/realm/realm-swift.git", branch: "master")
+        .package(url: "https://github.com/realm/realm-swift.git", from: "10.42.0")
         
     ],
     targets: [
